8b02743dc77639e64c8da953669e0623fc191e0d
Meikeb <m.h.r.bos@student.tudelft.nl>
2023-04-30 13:21:47 +0200

    Experimenten testen adv verstreken tijd

--- dataframe test.py
+++ dataframe test.py
@@ -1,10 +1,31 @@
 import pandas as pd
 
-raw_data = {"city": ['Tipoli', 'Rome', 'Sydney'],
-            "rank": ['3rd', '2nd', '1st'],
-            "name": ['Adam', 'Kevin', 'Pieter']}
-df = pd.DataFrame(raw_data)
-# filepath = r'C:/Users/mhrb0/PycharmProjects/MesaPractise/Results/Results_test'
-filepath = r'C:/Users/mhrb0/PycharmProjects/MesaPractise/Results/Results_test_new'
+# raw_data = {"city": ['Tipoli', 'Rome', 'Sydney'],
+#             "rank": ['3rd', '2nd', '1st'],
+#             "name": ['Adam', 'Kevin', 'Pieter']}
+# df = pd.DataFrame(raw_data)
+# # filepath = r'C:/Users/mhrb0/PycharmProjects/MesaPractise/Results/Results_test'
+# filepath = r'C:/Users/mhrb0/PycharmProjects/MesaPractise/Results/Results_test_new'
+#
+# df.to_csv(filepath)
 
-df.to_csv(filepath)
+
+for i in range(0,3):
+    search_pattern = "Parallel sweep"
+    num_units = 1
+    search_radius = 3
+    max_current = 10
+    time = 5
+    raw_data = {"Search pattern": [search_pattern],
+                "Num_units": [num_units],
+                "Search radius": [search_radius],
+                "Max_current": [max_current],
+                "Time": [time]}
+
+    print(f"running experiment {i}")
+    version = f'{i}'
+    df = pd.DataFrame(raw_data)
+    filepath = r'C:/Users/mhrb0/PycharmProjects/MesaPractise/Results/Results_test'+version
+
+    df.to_csv(filepath)
+#
--- main.py
+++ main.py
@@ -1,51 +1,63 @@
 from SarServer import server
-import pandas as pd
 from SarModel import SearchAndRescue
-# import gini
 
+import pandas as pd
+
+# Set to True if  you want to experiment and to False if you want to see the running server
+Experimenting = False
 # Run the server and do manual experiments
-server.launch()
 
-# Run a batch of experiments with different parameter values
-# for i in range(0,10):
-#     nr_steps = 0
-#
-#     width = 90
-#     height = 60
-#     search_pattern = "Parallel sweep"
-#     num_units = 1
-#     search_radius = 3
-#     max_current = 10
-#     model = SearchAndRescue(width, height, search_pattern, num_units, search_radius, max_current)
-#     while model.running or nr_steps <= 500:
-#         model.step()
-#         nr_steps += 1
-#
-#         gini.to_csv("model_data.csv")
-#
-#
-#     for agent in model.schedule.agents:
-#         all_wealth.append(agent.wealth)
+if not Experimenting:
+    server.launch()
+else:
+    time_till_drowning = 500
+    for i in range(0,5):
+        nr_steps = 0
 
-# results = "hallo"
-# results_df = pd.DataFrame(results)
-# results_df.to_csv(filepath)
-#
-# from pathlib import Path
-# filepath = Path('folder/subfolder/out.csv')
-# filepath.parent.mkdir(parents=True, exist_ok=True)
-# df.to_csv(filepath)
-#
-# res = {'name': ['Raphael', 'Donatello'],
-#                    'mask': ['red', 'purple'],
-#                    'weapon': ['sai', 'bo staff']}
-# df = pd.DataFrame(res)
-# df.to_csv(index=False)
+        width = 90
+        height = 60
+        search_pattern = "Parallel sweep"
+        num_units = 1
+        search_radius = 10
+        max_current = 2
+        model = SearchAndRescue(width, height, search_pattern, num_units, search_radius, max_current)
+        while model.running and nr_steps <= time_till_drowning:
+            model.step()
+            nr_steps += 1
+            print(nr_steps)
 
-# raw_data = {"city": ['Tripoli','Rome','Sydney'],
-#             "rank":['3rd','2nd','1st'],
-#             "name": ['Adam','Kevin','Pieter']}
-# # df = pd.DataFrame(raw_data, colums=['city','rank','name'])
-# df = pd.DataFrame(raw_data)
-# df.to_csv('C:/Users/mhrb0/OneDrive/Documenten/Delf/TB/BEP/Resultaten/test19-12.txt')
+        if nr_steps == time_till_drowning:
+            time = 0
+        else:
+            time = nr_steps
 
+        raw_data = {"Search pattern": [search_pattern],
+                    "Num_units": [num_units],
+                    "Search radius": [search_radius],
+                    "Max_current": [max_current],
+                    "Time": [time]}
+
+        version = f'{i}'
+        df = pd.DataFrame(raw_data)
+        filepath = r'C:/Users/mhrb0/PycharmProjects/MesaPractise/Results/Results_test' + version
+
+        df.to_csv(filepath)
+
+    # for i in range(0, 3):
+    #     search_pattern = "Parallel sweep"
+    #     num_units = 1
+    #     search_radius = 3
+    #     max_current = 10
+    #     time = 5
+    #     raw_data = {"Search pattern": [search_pattern],
+    #                 "Num_units": [num_units],
+    #                 "Search radius": [search_radius],
+    #                 "Max_current": [max_current],
+    #                 "Time": [time]}
+    #
+    #     print(f"running experiment {i}")
+    #     version = f'{i}'
+    #     df = pd.DataFrame(raw_data)
+    #     filepath = r'C:/Users/mhrb0/PycharmProjects/MesaPractise/Results/Results_test' + version
+    #
+    #     df.to_csv(filepath)
