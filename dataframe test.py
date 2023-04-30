2cc4192ed3218b8f640ac29b94b680820fe75094
Meikeb <m.h.r.bos@student.tudelft.nl>
2023-04-30 12:38:56 +0200

    Testing en heroppakken

--- dataframe test.py
+++ dataframe test.py
@@ -4,5 +4,7 @@ raw_data = {"city": ['Tipoli', 'Rome', 'Sydney'],
             "rank": ['3rd', '2nd', '1st'],
             "name": ['Adam', 'Kevin', 'Pieter']}
 df = pd.DataFrame(raw_data)
-filepath = r'C:/Users/mhrb0/PycharmProjects/MesaPractise/Results/Results_test'
+# filepath = r'C:/Users/mhrb0/PycharmProjects/MesaPractise/Results/Results_test'
+filepath = r'C:/Users/mhrb0/PycharmProjects/MesaPractise/Results/Results_test_new'
+
 df.to_csv(filepath)
