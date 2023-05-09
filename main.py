8773d2c808069ca1635600babbdac3a982b78d7c
Meikeb <m.h.r.bos@student.tudelft.nl>
2023-05-09 14:17:27 +0200

    1

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ from SarMissingPerson import MissingPerson
 
 import pandas as pd
 
-
-
 # Set to True if  you want to experiment and to False if you want to see the running server
 Experimenting = False
 single_seed = False
